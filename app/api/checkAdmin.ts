1f78e6fa7800496d77dffcb1898de314405e9fac
Matteo Benfatto <matteobenfatto97@gmail.com>
2024-09-21 20:31:58 +0200

    checkAdmin

--- app/api/checkAdmin.ts
+++ app/api/checkAdmin.ts
@@ -5,17 +5,25 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { email, password, adminCode } = req.body;
+  if (req.method === "POST") {
+    const { email, password, adminCode } = req.body;
 
-  try {
-    const admin = await checkAdminCredentials({ email, password, adminCode });
+    try {
+      const admin = await checkAdminCredentials({ email, password, adminCode });
 
-    if (admin) {
-      res.status(200).json({ success: true, redirectUrl: "/admin" });
-    } else {
-      res.status(401).json({ success: false });
+      if (admin) {
+        res.status(200).json({ success: true, redirectUrl: "/admin" });
+      } else {
+        res.status(401).json({ success: false });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
     }
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Internal server error" });
+  } else {
+    // Handle other methods, e.g., return 405 for non-POST methods
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
